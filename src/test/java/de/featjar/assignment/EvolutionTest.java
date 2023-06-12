d630f3516d46e52e4ba87607e11cab0c3767b83d
Pascal Schiessle <pascal.schiessle@web.de>
2023-06-12 15:11:56 +0200

    Remapping should work now

--- src/test/java/de/featjar/assignment/EvolutionTest.java
+++ src/test/java/de/featjar/assignment/EvolutionTest.java
@@ -23,7 +23,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -38,7 +37,7 @@ public class EvolutionTest {
     public void testTry() {
         ExtensionLoader.load();
         //ModelRepresentation repEvo0 = ModelRepresentation.load(Paths.get("C:/Users/gamef/Documents/GitHub/FeatJAR/formula-analysis-sat4j/src/test/resources/GPL/model.xml")).orElse(Logger::logProblems);
-        /*
+
         ModelRepresentation repEvo0 = ModelRepresentation.load(Paths.get(
                         "C:/Users/gamef/Documents/GitHub/FeatJAR/FeatJAR/formula-analysis-sat4j/src/test/resources/MA_PS/m_simple_e0.xml"))
                 .orElse(Logger::logProblems);
@@ -46,14 +45,12 @@ public class EvolutionTest {
                         "C:/Users/gamef/Documents/GitHub/FeatJAR/FeatJAR/formula-analysis-sat4j/src/test/resources/MA_PS/m_simple_e1.xml"))
                 .orElse(Logger::logProblems);
 
-         */
-        ModelRepresentation repEvo0 = ModelRepresentation.load(Paths.get(
+        /*ModelRepresentation repEvo0 = ModelRepresentation.load(Paths.get(
                         "C:/Users/gamef/Documents/GitHub/FeatJAR/FeatJAR/formula-analysis-sat4j/src/test/resources/MA_PS/model_evo0.xml"))
                 .orElse(Logger::logProblems);
         ModelRepresentation repEvo1 = ModelRepresentation.load(Paths.get(
                         "C:/Users/gamef/Documents/GitHub/FeatJAR/FeatJAR/formula-analysis-sat4j/src/test/resources/MA_PS/model_evo1_simple.xml"))
-                .orElse(Logger::logProblems);
-
+                .orElse(Logger::logProblems);*/
 
         // Evolution Step 0
         CNF cnfEvo0 = repEvo0.get(CNFProvider.fromFormula());
@@ -72,18 +69,18 @@ public class EvolutionTest {
         System.out.println("\nOLD COVERAGE (Should be 1.0) = " + oldCoverage + "\n");
 
         YASA yasa = new YASA();
-        yasa.setSolver(new Sat4JSolver(repEvo1.get(CNFProvider.fromFormula())));
+        yasa.setSolver(new Sat4JSolver(cnfEvo1));
         var monitor = new NullMonitor();
         yasa.init2(monitor);
 
         solutionList.getSolutions().forEach(s -> {
             System.out.println("############# SOLUTION START ###############");
-            var isValid = checkConfigurationOnNextEvolution(repEvo0, repEvo1, formulaEvo, s);
-            var oldConfiguration = IntStream.of(s.getLiterals()).filter(i -> i != 0).toArray();
-            var nextConfiguration = remapItemsByName(oldConfiguration, cnfEvo0, cnfEvo1, 0);
-            if (PRINT_CONFIG_EXTENDED) {
+            var isValid = checkConfigurationOnNextEvolution(repEvo1, formulaEvo, s);
+            var oldConfigurationWithZeros = IntStream.of(s.getLiterals()).toArray();
+            var nextConfiguration = remapItemsByName(oldConfigurationWithZeros, cnfEvo0, cnfEvo1);
+            if (PRINT_CONFIG_EXTENDED && !isValid) {
                 System.out.println("OLD CONFIG");
-                printConfigurationWithName(oldConfiguration, cnfEvo0);
+                printConfigurationWithName(oldConfigurationWithZeros, cnfEvo0);
                 System.out.println("NEXT CONFIG");
                 printConfigurationWithName(nextConfiguration, cnfEvo1);
             }
@@ -91,15 +88,120 @@ public class EvolutionTest {
             System.out.println("############## SOLUTION END ################");
         });
 
-        yasa.buildConfigurations(monitor);
         // New sample from partial configuration
+        yasa.buildConfigurations(monitor);
         var newSample = StreamSupport.stream(yasa, false).collect(Collectors.toCollection(ArrayList::new));
         System.out.println("\nNEW SAMPLE");
         System.out.println(newSample);
 
         // Calculate coverage
         var newSolutions = new SolutionList(repEvo1.getVariables(), newSample);
-        System.out.println("\nNEW COVERAGE = " + calculateCoverage(repEvo1.get(CNFProvider.fromFormula()), newSolutions) + " | Old Coverage = " + oldCoverage + "\n");
+        System.out.println("\nNEW COVERAGE = " + calculateCoverage(cnfEvo1, newSolutions) + " | Old Coverage = " + oldCoverage + "\n");
+    }
+
+    private static int[] remapItemsByName(int[] oldConfigurationWithZeros, CNF cnfOld, CNF cnfNext) {
+        var nextAssignment = new ArrayList<Integer>();
+        List<Integer> oldConfigAsList = IntStream.of(oldConfigurationWithZeros).boxed().collect(Collectors.toList());
+
+        for (var nextIndex = 1; nextIndex <= cnfNext.getVariableMap().getVariableCount(); nextIndex++) {
+            // get current var name
+            var nextVarNameOpt = cnfNext.getVariableMap().getVariableName(nextIndex);
+            if (nextVarNameOpt.isEmpty()) continue;
+
+            // did feature exist in old CNF? if no, ignore
+            var nextVarName = nextVarNameOpt.get();
+            var oldVarIndexOpt = cnfOld.getVariableMap().getVariableIndex(nextVarName);
+            if (oldVarIndexOpt.isEmpty()) continue; // didn´t exist, go on
+
+            // did exist, do we consider it?
+            var oldVarIndex = oldVarIndexOpt.get();
+            // check if the old index appears in the oldConfig (if set to zero also not consider it)
+            var oldValueOpt = oldConfigAsList.stream().filter(i -> Math.abs(i) == oldVarIndex).findFirst();
+            if (oldValueOpt.isEmpty()) continue;
+
+            // otherwise add the old value to the new assignment
+            var oldValue = oldValueOpt.get();
+            nextAssignment.add(oldValue > 0 ? nextIndex : -nextIndex);
+        }
+        return nextAssignment.stream().mapToInt(i -> i).toArray();
+    }
+
+    private static int getOldOrDefaultValue(int nextIndex, List<Integer> oldAssignment, CNF cnfOld, CNF cnfNext) {
+        var defaultValue = 0;
+        var nextVarName = cnfNext.getVariableMap().getVariableName(nextIndex + 1);
+        if (nextVarName.isEmpty()) return defaultValue;
+        var oldIndex = cnfOld.getVariableMap().getVariableIndex(nextVarName.get());
+        if (oldIndex.isEmpty()) return defaultValue;
+        for (int oldValue :
+                oldAssignment) {
+            if (oldValue == oldIndex.get() || oldValue == -oldIndex.get()) {
+                return oldValue;
+            }
+        }
+        return defaultValue;
+    }
+
+    private static boolean checkConfigurationOnNextEvolution(ModelRepresentation repEvo, Formula formula, LiteralList s) {
+        List<LiteralList> assumptions = new ArrayList<>();
+        ContradictionAnalysis contra = new ContradictionAnalysis();
+
+        System.out.println(Arrays.toString(s.getLiterals()));
+        IndexAssignment a = new IndexAssignment();
+        for (int l : s.getLiterals()) {
+            a.set(Math.abs(l), l > 0);
+            assumptions.add(new LiteralList(l));
+        }
+        contra.setClauseList(assumptions);
+
+        boolean isFormulaValid = (boolean) Formulas.evaluate(formula, a).orElse(false);
+        System.out.println("Is configuration valid = " + isFormulaValid);
+
+        if (isFormulaValid) return true;
+
+        formula.getChildren().forEach(clause -> {
+            boolean isValid = (boolean) Formulas.evaluate(clause, a).orElse(false);
+            if (!isValid) {
+                Formulas.getVariables(clause).forEach(variable -> {
+                    s.getLiterals()[variable.getIndex() - 1] = 0;
+                });
+            }
+        });
+
+        HasSolutionAnalysis sat = new HasSolutionAnalysis();
+        Assignment assumptions2 = contra.getAssumptions();
+        for (int l : s.getLiterals()) {
+            assumptions2.set(Math.abs(l), l > 0);
+        }
+        assumptions2 = sat.getAssumptions();
+        for (int l : s.getLiterals()) {
+            if (l != 0) {
+                assumptions2.set(Math.abs(l), l > 0);
+            }
+        }
+        Boolean canBeValid = repEvo.get(sat);
+        System.out.println(s);
+        System.out.println("Is CNF satisfiable? CanBeValid = " + canBeValid + "\n");
+        return false;
+    }
+
+    private static double calculateCoverage(CNF cnf, SolutionList solutionList) {
+        TWiseConfigurationUtil util = new TWiseConfigurationUtil(cnf, new Sat4JSolver(cnf));
+        TWiseStatisticGenerator stat = new TWiseStatisticGenerator(util);
+
+        PresenceConditionManager pcManager = new PresenceConditionManager(
+                util.getDeadCoreFeatures(),
+                solutionList.getVariableMap().getVariableCount(),
+                TWiseConfigurationGenerator.convertLiterals(Clauses.getLiterals(cnf.getVariableMap())));
+        var coverage = stat.getCoverage(List.of(solutionList.getSolutions()),
+                pcManager.getGroupedPresenceConditions(), 2
+                , TWiseStatisticGenerator.ConfigurationScore.NONE, true);
+        return coverage.get(0).getCoverage();
+    }
+
+    private static SolutionList generateValidTWiseConfigurations(ModelRepresentation rep) {
+        TWiseConfigurationGenerator twisegen = new TWiseConfigurationGenerator();
+        twisegen.setT(2);
+        return rep.get(twisegen);
     }
 
     private static void printCNF(CNF cnf) {
@@ -129,109 +231,5 @@ public class EvolutionTest {
         builder.append("\n");
         builder.append("+++++++++CONFIGURATION++++++++++++++++++\n");
         System.out.println(builder);
-    }
-
-    private static int[] remapItemsByName(int[] oldConfiguration, CNF cnfOld, CNF cnfNext, int defaultValue) {
-        var nextAssignment = new int[cnfNext.getVariableMap().getVariableCount()];
-        for (var nextIndex = 0; nextIndex < cnfNext.getVariableMap().getVariableCount(); nextIndex++) {
-            nextAssignment[nextIndex] = (int) getOldOrDefaultValue(nextIndex, oldConfiguration, cnfOld, cnfNext, defaultValue);
-        }
-        return Arrays.stream(nextAssignment).filter(i -> i != 0).toArray();
-    }
-
-    private static Object getOldOrDefaultValue(int nextIndex, int[] oldAssignment, CNF cnfOld, CNF cnfNext, Object defaultValue) {
-        var nextVarName = cnfNext.getVariableMap().getVariableName(nextIndex + 1);
-        if (nextVarName.isEmpty()) return defaultValue;
-        var oldIndex = cnfOld.getVariableMap().getVariableIndex(nextVarName.get());
-        if (oldIndex.isEmpty()) return defaultValue;
-        return oldAssignment[oldIndex.get() - 1] > 0 ? nextIndex + 1 : -(nextIndex + 1);
-    }
-
-    /**
-     * Indexassignment remapping First, all
-     *
-     * @param oldAssignment  prior assignment that should be remapped
-     * @param indexRemapping key is the new index position, the value is the old position
-     * @param newItemsToAdd  key is the position where we should add the elements, value the value to
-     *                       add
-     * @return remapped assignment
-     */
-    private static IndexAssignment remapItems(IndexAssignment oldAssignment,
-                                              Map<Integer, Integer> indexRemapping, Map<Integer, Object> newItemsToAdd) {
-
-        var assignment = new IndexAssignment();
-        //TODO: Check if mapping is valid, no duplicates, values present etc.
-        indexRemapping.forEach(
-                (newI, oldI) -> oldAssignment.get(oldI).ifPresent((value) -> assignment.set(newI, value)));
-        newItemsToAdd.forEach(assignment::set);
-
-        return assignment;
-    }
-
-    private static boolean checkConfigurationOnNextEvolution(ModelRepresentation rep,
-                                                             ModelRepresentation repEvo, Formula formula, LiteralList s) {
-        List<LiteralList> assumptions = new ArrayList<>();
-        ContradictionAnalysis contra = new ContradictionAnalysis();
-
-        System.out.println(Arrays.toString(s.getLiterals()));
-        IndexAssignment a = new IndexAssignment();
-        for (int l : s.getLiterals()) {
-            a.set(Math.abs(l), l > 0);
-            assumptions.add(new LiteralList(l));
-        }
-        contra.setClauseList(assumptions);
-
-        boolean isFormulaValid = (boolean) Formulas.evaluate(formula, a)
-                .orElse(false); //TODO: orElse false gleich richtig? Was bedeuetet no value present here?
-        System.out.println("Is configuration valid = "
-                + isFormulaValid); //evaluate => search for solution, each solution = one configuration
-        if (isFormulaValid) return isFormulaValid;
-
-        formula.getChildren().forEach(clause -> {
-            boolean isValid = (boolean) Formulas.evaluate(clause, a)
-                    .orElse(false); //TODO: orElse false gleich richtig? Was bedeuetet no value present here?
-            if (!isValid) {
-                Formulas.getVariables(clause).forEach(variable -> {
-                    s.getLiterals()[variable.getIndex() - 1] = 0;
-                });
-
-            }
-        });
-
-        HasSolutionAnalysis sat = new HasSolutionAnalysis();
-        Assignment assumptions2 = contra.getAssumptions();
-        for (int l : s.getLiterals()) {
-            assumptions2.set(Math.abs(l), l > 0);
-        }
-        assumptions2 = sat.getAssumptions();
-        for (int l : s.getLiterals()) {
-            if (l != 0) {
-                assumptions2.set(Math.abs(l), l > 0);
-            }
-        }
-        Boolean canBeValid = repEvo.get(sat);
-        System.out.println(s);
-        System.out.println("Is CNF satisfiable? CanBeValid = " + canBeValid + "\n");
-        return isFormulaValid;
-    }
-
-    private static double calculateCoverage(CNF cnf, SolutionList solutionList) {
-        TWiseConfigurationUtil util = new TWiseConfigurationUtil(cnf, new Sat4JSolver(cnf));
-        TWiseStatisticGenerator stat = new TWiseStatisticGenerator(util);
-
-        PresenceConditionManager pcManager = new PresenceConditionManager(
-                util.getDeadCoreFeatures(),
-                solutionList.getVariableMap().getVariableCount(),
-                TWiseConfigurationGenerator.convertLiterals(Clauses.getLiterals(cnf.getVariableMap())));
-        var coverage = stat.getCoverage(List.of(solutionList.getSolutions()),
-                pcManager.getGroupedPresenceConditions(), 2
-                , TWiseStatisticGenerator.ConfigurationScore.NONE, true);
-        return coverage.get(0).getCoverage();
-    }
-
-    private static SolutionList generateValidTWiseConfigurations(ModelRepresentation rep) {
-        TWiseConfigurationGenerator twisegen = new TWiseConfigurationGenerator();
-        twisegen.setT(2);
-        return rep.get(twisegen);
     }
 }
